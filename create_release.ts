de44697826c3a0c0618ef6c68563ccf0845b3314
Roj <rojvv@icloud.com>
2023-06-09 17:24:24 +0000

    Update create_release.ts

--- create_release.ts
+++ create_release.ts
@@ -2,6 +2,8 @@ import $ from "https://deno.land/x/dax@0.31.1/mod.ts";
 
 const v = Deno.args[0];
 
+if (!v.startsWith("0")) throw new Error("Version not allowed");
+
 const content = Deno.readTextFileSync("constants.ts");
 
 Deno.writeTextFileSync("constants.ts", content.replace(/(MTKruto )[0-9]+.[0-9]+.[0-9]+(";\n)/, `$1${v}$2`));
