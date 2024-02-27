4ca0c0207650af687fc5f4e550639910f2cc99f8
Roj <ez@roj.im>
2024-02-27 15:57:17 +0300

    Create pre_jsr.ts

---
+++ pre_jsr.ts
@@ -0,0 +1,17 @@
+import { parse } from "https://deno.land/std@0.217.0/jsonc/mod.ts";
+
+const version = Deno.args[0];
+
+if (!version.startsWith("0")) throw new Error("Version not allowed");
+
+
+// deno-lint-ignore no-explicit-any
+const config = parse(Deno.readTextFileSync("deno.jsonc")) as any;
+Deno.writeTextFileSync(
+  "deno.jsonc",
+  JSON.stringify(
+    { name: "@mtkruto/mtkruto", version, exports: './mod.ts', ...config },
+    null,
+    2,
+  ),
+);
