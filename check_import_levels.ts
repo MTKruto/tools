e7187d7eae4ce270a5c26f918e8b0ae05c93aa7d
Roj <ez@roj.im>
2024-04-04 11:35:22 +0300

    Add exception

--- check_import_levels.ts
+++ check_import_levels.ts
@@ -3,31 +3,56 @@ import { red } from "https://deno.land/std@0.221.0/fmt/colors.ts";
 import { Project } from "https://deno.land/x/ts_morph@22.0.0/mod.ts";
 
 function getSourceFiles(path: string) {
-  return [...Deno.readDirSync(path)].filter((v) => v.isFile && v.name.endsWith(".ts")).map((v) => join(path, v.name));
+  return [...Deno.readDirSync(path)].filter((v) =>
+    v.isFile && v.name.endsWith(".ts")
+  ).map((v) => join(path, v.name));
 }
 
 const cwd = Deno.cwd();
-const directories = [...Deno.readDirSync(cwd)].filter((v) => v.isDirectory).map((v) => join(cwd, v.name));
+const directories = [...Deno.readDirSync(cwd)].filter((v) => v.isDirectory).map(
+  (v) => join(cwd, v.name),
+);
 
-// ==== CHECK IMPORT LEVELS ==== /
+// ==== CHECK IMPORT LEVELS ==== //
 let violationCount = 0;
 const project = new Project();
-for (const file of getSourceFiles(cwd).concat(...directories.map(getSourceFiles))) {
-  const thisLevel = Number(basename(file).split("_")[0]);
+for (
+  const file of getSourceFiles(cwd).concat(...directories.map(getSourceFiles))
+) {
+  const p = basename(file).split("_")[0];
+  if (!p) {
+    continue;
+  }
+  const thisLevel = Number(p);
   if (isNaN(thisLevel)) {
     continue;
   }
 
   const sourceFile = project.addSourceFileAtPath(file);
   const relativeImportLevels = sourceFile.getExportDeclarations()
-    .map((v) => [v.getModuleSpecifier()?.getLiteralText(), v.getStartLineNumber()])
-    .concat(sourceFile.getImportDeclarations().map((v) => [v.getModuleSpecifier().getLiteralText(), v.getStartLineNumber()]))
+    .map((
+      v,
+    ) => [v.getModuleSpecifier()?.getLiteralText(), v.getStartLineNumber()])
+    .concat(
+      sourceFile.getImportDeclarations().map((
+        v,
+      ) => [v.getModuleSpecifier().getLiteralText(), v.getStartLineNumber()]),
+    )
     .filter((v): v is [string, number] => !!v[0])
     .filter((v) => v[0].startsWith("./"))
-    .map((v) => [Number(v[0].slice(2).split("_")[0]), v[1]])
+    .map((v) => {
+      const p = v[0].slice(2).split("_")[0];
+      if (!p) {
+        return [NaN, v[1]];
+      }
+      return [p, v[1]];
+    })
     .filter((v): v is [number, number] => !isNaN(v[0]));
   project.removeSourceFile(sourceFile);
   for (const [l, line] of relativeImportLevels) {
+    if (l == thisLevel && file.endsWith("_test.ts")) {
+      continue; // exception
+    }
     if (l >= thisLevel) {
       console.log(red("Violation"), file + ":" + line);
       ++violationCount;
@@ -35,7 +60,11 @@ for (const file of getSourceFiles(cwd).concat(...directories.map(getSourceFiles)
   }
 }
 
-console.log(violationCount == 0 ? "No" : violationCount + "", "violation" + (violationCount == 1 ? "" : "s"), "were found.");
+console.log(
+  violationCount == 0 ? "No" : violationCount + "",
+  "violation" + (violationCount == 1 ? "" : "s"),
+  "were found.",
+);
 if (violationCount) {
   Deno.exit(1);
 }
