d7731dd504eddac37b758ad69e933015a0f01e24
Roj <ez@roj.im>
2024-04-04 11:30:43 +0300

    Update import level checker

---
+++ check_import_levels.ts
@@ -0,0 +1,41 @@
+import { basename, join } from "https://deno.land/std@0.221.0/path/mod.ts";
+import { red } from "https://deno.land/std@0.221.0/fmt/colors.ts";
+import { Project } from "https://deno.land/x/ts_morph@22.0.0/mod.ts";
+
+function getSourceFiles(path: string) {
+  return [...Deno.readDirSync(path)].filter((v) => v.isFile && v.name.endsWith(".ts")).map((v) => join(path, v.name));
+}
+
+const cwd = Deno.cwd();
+const directories = [...Deno.readDirSync(cwd)].filter((v) => v.isDirectory).map((v) => join(cwd, v.name));
+
+// ==== CHECK IMPORT LEVELS ==== /
+let violationCount = 0;
+const project = new Project();
+for (const file of getSourceFiles(cwd).concat(...directories.map(getSourceFiles))) {
+  const thisLevel = Number(basename(file).split("_")[0]);
+  if (isNaN(thisLevel)) {
+    continue;
+  }
+
+  const sourceFile = project.addSourceFileAtPath(file);
+  const relativeImportLevels = sourceFile.getExportDeclarations()
+    .map((v) => [v.getModuleSpecifier()?.getLiteralText(), v.getStartLineNumber()])
+    .concat(sourceFile.getImportDeclarations().map((v) => [v.getModuleSpecifier().getLiteralText(), v.getStartLineNumber()]))
+    .filter((v): v is [string, number] => !!v[0])
+    .filter((v) => v[0].startsWith("./"))
+    .map((v) => [Number(v[0].slice(2).split("_")[0]), v[1]])
+    .filter((v): v is [number, number] => !isNaN(v[0]));
+  project.removeSourceFile(sourceFile);
+  for (const [l, line] of relativeImportLevels) {
+    if (l >= thisLevel) {
+      console.log(red("Violation"), file + ":" + line);
+      ++violationCount;
+    }
+  }
+}
+
+console.log(violationCount == 0 ? "No" : violationCount + "", "violation" + (violationCount == 1 ? "" : "s"), "were found.");
+if (violationCount) {
+  Deno.exit(1);
+}
