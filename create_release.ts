4326779ccf4682a704c66798d15d69cfb5cca97c
Roj <rojvv@icloud.com>
2024-04-04 18:31:01 +0300

    Update create_release.ts

--- create_release.ts
+++ create_release.ts
@@ -4,15 +4,6 @@ const v = Deno.args[0];
 
 if (!v.startsWith("0")) throw new Error("Version not allowed");
 
-const content = Deno.readTextFileSync("4_constants.ts");
-
-Deno.writeTextFileSync(
-  "4_constants.ts",
-  content.replace(/(MTKruto )[0-9]+.[0-9]+.[0-9]+(";\n)/, `$1${v}$2`),
-);
-
-await $`git add 4_constants.ts`;
-await $`git commit -m ${v}`;
 await $`git tag ${v} -m ${v}`;
 await $`git push`;
 await $`git push origin ${v}`;
