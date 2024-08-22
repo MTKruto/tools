cd5c03596d530c6590eb2150a96d5eafba92fc2c
Roj <rojvv@icloud.com>
2024-08-22 12:02:09 +0300

    Update pre_jsr.ts

--- pre_jsr.ts
+++ pre_jsr.ts
@@ -9,7 +9,7 @@ const config = parse(Deno.readTextFileSync("deno.jsonc")) as any;
 Deno.writeTextFileSync(
   "deno.jsonc",
   JSON.stringify(
-    { name: "@mtkruto/mtkruto", version, exports: "./mod.ts", ...config },
+    { name: "@mtkruto/mtkruto", version, license: "LGPL-3.0-or-later", exports: "./mod.ts", ...config },
     null,
     2,
   ),
