5bef928e70d924b4ecca31b8908e7f7a0ccd0cb8
Roj <rojvv@icloud.com>
2025-07-22 03:37:28 +0300

    Add extra checks

--- check_import_levels.ts
+++ check_import_levels.ts
@@ -11,38 +11,76 @@ for (const file of iterSourceFiles()) {
   if (!p) {
     continue;
   }
-  const thisLevel = Number(p);
-  if (isNaN(thisLevel)) {
-    continue;
-  }
+  const thisLevel = p;
 
   const sourceFile = project.addSourceFileAtPath(file);
-  const relativeImportLevels = sourceFile.getExportDeclarations()
+  if (file === "mod.ts" || file.endsWith("/mod.ts")) {
+    continue;
+  }
+  interface RIL {
+    spec: string;
+    line: number;
+    export: boolean;
+    level: string;
+  }
+  const relativeImportLevels: RIL[] = sourceFile.getExportDeclarations()
     .map((
       v,
-    ) => [v.getModuleSpecifier()?.getLiteralText(), v.getStartLineNumber()])
+    ) => ({
+      spec: v.getModuleSpecifier()?.getLiteralText(),
+      line: v.getStartLineNumber(),
+      export: true,
+    }))
     .concat(
       sourceFile.getImportDeclarations().map((
         v,
-      ) => [v.getModuleSpecifier().getLiteralText(), v.getStartLineNumber()]),
+      ) => ({
+        spec: v.getModuleSpecifier()?.getLiteralText(),
+        line: v.getStartLineNumber(),
+        export: false,
+      })),
     )
-    .filter((v): v is [string, number] => !!v[0])
-    .filter((v) => v[0].startsWith("./"))
-    .map((v) => {
-      const p = v[0].slice(2).split("_")[0];
+    .filter((v): v is Omit<RIL, "level"> => !!v.spec)
+    .filter((v) => v.spec.startsWith("./"))
+    .filter((v) => v.spec.split("/").length === 2)
+    .map((v): RIL | false => {
+      const p = v.spec.split("/").slice(-1)[0].split("_")[0];
       if (!p) {
-        return [NaN, v[1]];
+        return false;
       }
-      return [p, v[1]];
+      return { ...v, level: p };
     })
-    .filter((v): v is [number, number] => !isNaN(v[0] as number));
+    .filter((v): v is RIL => !!v);
   project.removeSourceFile(sourceFile);
-  for (const [l, line] of relativeImportLevels) {
-    if (l == thisLevel && file.endsWith("_test.ts")) {
+  for (const i of relativeImportLevels) {
+    if (i.level === thisLevel && file.endsWith("_test.ts")) {
       continue; // exception
     }
-    if (l >= thisLevel) {
-      console.log(red("Violation"), file + ":" + line);
+    if (i.level.charCodeAt(0) >= thisLevel.charCodeAt(0)) {
+      console.log(red("Violation"), file + ":" + i.line);
+      ++violationCount;
+    }
+  }
+  const high = relativeImportLevels.sort((a, b) =>
+    b.level.charCodeAt(0) - a.level.charCodeAt(0)
+  )[0];
+  if (
+    high !== undefined &&
+    (high.level.charCodeAt(0) + 1 !== thisLevel.charCodeAt(0))
+  ) {
+    if (file.endsWith("_test.ts")) {
+      continue; // exception
+    }
+    console.log(red("Violation"), file + ":" + high.line);
+    ++violationCount;
+  }
+
+  const counts: Record<string, number> = {};
+  for (const i of relativeImportLevels) {
+    counts[i.spec + i.export] ??= 0;
+    ++counts[i.spec + i.export];
+    if (counts[i.spec + i.export] > 1) {
+      console.log(red("Violation"), file + ":" + i.line, "(" + i.spec + ")");
       ++violationCount;
     }
   }
@@ -51,7 +89,7 @@ for (const file of iterSourceFiles()) {
 console.log(
   violationCount == 0 ? "No" : violationCount + "",
   "violation" + (violationCount == 1 ? " was" : "s were"),
-  " found.",
+  "found.",
 );
 if (violationCount) {
   Deno.exit(1);
